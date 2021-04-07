62b12f650512c54db75782f6d333de02254b8ab2
VernieThorpe <verniethorpe@VT-MacBook.local>
2021-04-08 03:31:47 +0530

    yay

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Drawer } from "./components";
 //import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Drawer />
+
       <h1>hello world</h1>
     </div>
   );
